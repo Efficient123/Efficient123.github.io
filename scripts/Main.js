641419609d314b7cb16b043018135de9755eeb76
Efficient123 <efficient0123@gmail.com>
2024-11-16 21:13:09 +0530

    Add files via upload

--- scripts/Main.js
+++ scripts/Main.js
@@ -2,17 +2,17 @@ document.addEventListener("DOMContentLoaded", () => {
     const quoteElement = document.querySelector('.qoute');
     const quotes = ['Design. Develop. Deliver.', 'Code. Create. Conquer.'];
 
-    // Set the initial quote right when the page loads
+    
     let currentQuote = quotes[Math.floor(Math.random() * quotes.length)];
     quoteElement.textContent = currentQuote;
 
-    // Start changing the quote after a short delay
+    
     function changeQuote() {
         let newQuote;
         do {
             newQuote = quotes[Math.floor(Math.random() * quotes.length)];
-        } while (newQuote === currentQuote); // Ensure the new quote is different
-        currentQuote = newQuote; // Update the current quote
+        } while (newQuote === currentQuote); 
+        currentQuote = newQuote; 
         quoteElement.textContent = newQuote;
     }
 
